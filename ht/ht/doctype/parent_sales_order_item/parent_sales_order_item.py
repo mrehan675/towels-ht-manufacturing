a572f5c5dc340858835edcddd54137f1f763362e
mrehan <rehan.muhamad675@gmail.com>
2025-01-21 14:18:45 +0000

    SO item table issue

---
+++ ht/ht/doctype/parent_sales_order_item/parent_sales_order_item.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, SRP and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class ParentSalesOrderItem(Document):
+	pass
